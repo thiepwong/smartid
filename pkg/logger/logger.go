b09d465c972ac1fee14af65669fc7d9d94cb08cf
thiepwong <thiep.wong@gmail.com>
2019-03-13 18:25:44 +0700

    no message

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -14,21 +14,46 @@ var (
 
 func init() {
 	// set location of log file
-	infoPath, debugPath, errPath := "info.log", "debug.log", "error.log"
+	var (
+		infofile, errfile, debugfile *os.File
+		err1, err2, err3             error
+	)
 
-	var debugfile, err1 = os.Create(debugPath)
-	var infofile, err2 = os.Create(infoPath)
-	var errfile, err3 = os.Create(errPath)
+	infoPath, debugPath, errPath := "logs/info.log", "logs/debug.log", "logs/error.log"
+
+	os.Mkdir("logs", os.ModePerm)
+	if _, err := os.Stat(infoPath); os.IsNotExist(err) {
+		infofile, err2 = os.Create(infoPath)
+		LogInfo = log.New(infofile, "", log.Ldate+log.Ltime)
+		LogInfo.Println("Info : " + debugPath)
+
+	} else {
+		infofile, err2 = os.OpenFile(infoPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		LogInfo = log.New(infofile, "", log.Ldate+log.Ltime)
+	}
+
+	if _, err := os.Stat(errPath); os.IsNotExist(err) {
+		errfile, err3 = os.Create(errPath)
+
+		LogErr = log.New(errfile, "", log.Ldate+log.Ltime)
+		LogErr.Println("Error : " + errPath)
+	} else {
+		errfile, err2 = os.OpenFile(errPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		LogErr = log.New(errfile, "", log.Ldate+log.Ltime)
+	}
+
+	if _, err := os.Stat(debugPath); os.IsNotExist(err) {
+		debugfile, err1 = os.Create(debugPath)
+		LogDebug = log.New(debugfile, "", log.Ldate+log.Ltime)
+		LogDebug.Println("Debug : " + debugPath)
+	} else {
+		debugfile, err2 = os.OpenFile(debugPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		LogDebug = log.New(debugfile, "", log.Ldate+log.Ltime)
+	}
+
 	if err1 != nil || err2 != nil || err3 != nil {
 		fmt.Println("Loi moi ", err1, err2, err3)
 		panic(err1)
 	}
-	LogInfo = log.New(debugfile, "", log.Ldate+log.Ltime)
-	LogInfo.Println("Debug : " + debugPath)
 
-	LogInfo = log.New(infofile, "", log.Ldate+log.Ltime)
-	LogInfo.Println("Info : " + debugPath)
-
-	LogInfo = log.New(errfile, "", log.Ldate+log.Ltime)
-	LogInfo.Println("Error : " + debugPath)
 }
